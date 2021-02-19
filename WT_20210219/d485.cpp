c2be2eaceaf798dc24c54b6b07f32015dd451e45
yoyoisaman <66879339+yoyoisaman@users.noreply.github.com>
2021-02-19 12:01:34 +0800

    Update d485.cpp

--- WT_20210219/d485.cpp
+++ WT_20210219/d485.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 void sol(int a,int b){
-    a += (a%2);
-    b -= (b%2);
-    cout << (b-a)/2+1 << '\n';
+	a += (a%2);
+	b -= (b%2);
+	cout << (b-a)/2+1 << '\n';
 }
 int main(){
 	int a=0,b=0;
