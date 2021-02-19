b6e389d639bd8e4905ad17e91b0f9d0128f0375b
yoyoisaman <66879339+yoyoisaman@users.noreply.github.com>
2021-02-19 20:15:22 +0800

    Update b563.cpp

--- WT_20210219/b563.cpp
+++ WT_20210219/b563.cpp
@@ -3,22 +3,22 @@
 using namespace std;
  
 int main() {
-    map <int, map<int,int> > mp;
-    int n;
-    int a, b;
-    while (cin >> n){
-        int ans = 0;
-        for (int i = 0; i < n; i++){
-            cin >> a >> b;
-            if (mp[b][a]>0){
-                mp[b][a] -= 1;
-                mp[a][b] -= 1;
-		ans += 1;
-            }else{
-                mp[a][b] += 1;   
-            }
-        }
+	map <int, map<int,int> > mp;
+	int n;
+	int a, b;
+	while (cin >> n){
+		int ans = 0;
+		for (int i = 0; i < n; i++){
+			cin >> a >> b;
+			if (mp[b][a]>0){
+				mp[b][a] -= 1;
+				mp[a][b] -= 1;
+				ans += 1;
+			}else{
+				mp[a][b] += 1;   
+ 			}
+		}
         cout << ans << '\n';
-    }
+	}
 }
 
